88190696f5b4c24db8de4cfe4a29fe1177194950
Shrikanth Narayanan <shrikanth2@gmail.com>
2016-03-09 17:56:26 -0800

    adding survey to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { items } from './items';
 import { charts } from './charts';
+import { survey } from './survey';
 
 const rootReducer = combineReducers({
   form: formReducer,
   /* your reducers */
   items,
   charts,
+  survey,
 });
 
 export default rootReducer;
