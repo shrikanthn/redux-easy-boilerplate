b316ffe1c1dd1adcdcc28c980e1ac5a3d2a5e542
Shrikanth Narayanan <shrikanth2@gmail.com>
2016-03-10 11:40:53 -0800

    adding default params to bar chart

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -6,16 +6,20 @@ export class BarChart extends Component {
 
     static propTypes = {
         barChart: React.PropTypes.array,
+        title: React.PropTypes.string,
+        color: React.PropTypes.string,
+        width: React.PropTypes.number,
     };
 
     initChart() {
         if (!!this.props.barChart && this.props.barChart.length > 1) {
+
             return ( <D3.BarChart
 				data={this.props.barChart}
-				width={400}
+				width={this.props.width || 400}
 				height={200}
-				fill={'#3182bd'}
-				title='Bar Chart' />)
+				fill={this.props.color || '#3182bd'}
+				title={this.props.title || 'Bar Chart'} />)
         } else {
             return
         }
