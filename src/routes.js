b8949a8b89fd3e270285888154ce80998312e9d2
Shrikanth Narayanan <shrikanth2@gmail.com>
2016-03-10 15:24:23 -0800

    routing to survey results

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,14 @@ import { App } from 'containers/App';
 import { Home } from 'containers/Home';
 import { List } from 'containers/List';
 import { TestDB } from 'containers/TestDB';
+import { SurveyResults } from 'containers/SurveyResults';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="list" component={List} />
     <Route path="testdb" component={TestDB} />
+    <Route path="surveyResults" component={SurveyResults} />
     <Route status={404} path="*" component={Home} />
   </Route>
 );
