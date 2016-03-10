f0b5766d1b4986879305bbc593a55f5dc9cba8bd
Shrikanth Narayanan <shrikanth2@gmail.com>
2016-03-10 11:40:05 -0800

    adding data format method to DB utils

--- src/utils/DBUtils.js
+++ src/utils/DBUtils.js
@@ -1,9 +1,9 @@
 export function DBUtils() {
 
-	const sqlite3 = require('sqlite3').verbose();
-	const path = require('path');
-	const dbPath = path.resolve('./retroDB.db');
-	console.log('DBPath : ' + dbPath);
+    const sqlite3 = require('sqlite3').verbose();
+    const path = require('path');
+    const dbPath = path.resolve('./retroDB.db');
+    console.log('DBPath : ' + dbPath);
 
     this.getDB = function() {
         return new sqlite3.Database(dbPath);
@@ -15,13 +15,28 @@ export function DBUtils() {
         res.setHeader('Content-Type', 'application/json');
         db.all("SELECT * FROM answers", function(err, row) {
             if (!err) {
-                console.log(row);
+                console.log(row.length);
                 res.send(row);
             } else {
                 res.send(err);
             }
         });
         db.close();
+    };
+
+    this.processAvgData = function(rows) {
+        let data = [];
+        for (let i in rows) {
+            let obj1 = rows[i];
+            data.push({
+                a1: obj1.fun,
+                a2: obj1.performance,
+                a3: obj1.planning,
+                a4: obj1.focus,
+                ts: obj1.sprint_date,
+            });
+        }
+        return data;
     };
 
 
@@ -34,6 +49,7 @@ export function DBUtils() {
                 res.send(row);
             } else {
                 res.send(err);
+                console.log(err);
             }
         });
         db.close();
@@ -43,11 +59,16 @@ export function DBUtils() {
     this.getTeamAverages = function(res) {
 
         let db = this.getDB();
+        let processData = this.processAvgData;
         db.all("SELECT * FROM team_average", function(err, row) {
             res.setHeader('Content-Type', 'application/json');
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             if (!err) {
-                res.send(row);
+                console.log('Sending rows : ' + row.length);
+                res.send(processData(row));
             } else {
+                console.log(err);
                 res.send(err);
             }
         });
