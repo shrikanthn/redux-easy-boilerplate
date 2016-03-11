158a9bc7d3bc2f5570c94f09c29b4dbeba787033
Shrikanth Narayanan <shrikanth2@gmail.com>
2016-03-10 23:51:36 -0800

    code cleanup

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-/* global styles for app */
-import './styles/app.scss';
-
 export class App extends Component {
   static propTypes = {
     children: React.PropTypes.any,
