0a2a8decd2ab881e93c904a04f8146b809292e8e
Shrikanth Narayanan <shrikanth2@gmail.com>
2016-03-11 12:18:31 -0800

    routing teamAverages container

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,8 +13,8 @@ export class Header extends Component {
                   <Link to="/home" className="btn btn-default navbar-btn">Home </Link>
                   <Link to="/list" className="btn btn-default navbar-btn"> Sample Redux</Link>
                   <Link to="/testdb" className="btn btn-default navbar-btn"> Redux-TestDB</Link>
-                  <Link to="/surveyResults" className="btn btn-default navbar-btn"> Survey Results
-                  </Link>
+                  <Link to="/surveyResults" className="btn btn-default navbar-btn"> Survey Results</Link>
+                  <Link to="/teamAverage" className="btn btn-default navbar-btn"> Team Average</Link>
               </div>
           </div>
       </nav>
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-/* containers */
 import { App } from 'containers/App';
 import { Home } from 'containers/Home';
 import { List } from 'containers/List';
 import { TestDB } from 'containers/TestDB';
 import { SurveyResults } from 'containers/SurveyResults';
+import { TeamAverage } from 'containers/TeamAverage';
 
 export default (
   <Route path="/" component={App}>
@@ -14,6 +14,7 @@ export default (
     <Route path="list" component={List} />
     <Route path="testdb" component={TestDB} />
     <Route path="surveyResults" component={SurveyResults} />
+    <Route path="teamAverage" component={TeamAverage} />
     <Route status={404} path="*" component={Home} />
   </Route>
 );
